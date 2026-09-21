d676625372ead454a1e9f80490564c6144fb6585
Gellory <43223019+Gellory@users.noreply.github.com>
2026-09-21 05:26:58 +0300

    Add files via upload

--- events/Join/guildMemberAdd.js
+++ events/Join/guildMemberAdd.js
@@ -1,1 +1,58 @@
+/*
+const db = require("pro.db");
+const inviteTracker = require("invite-tracker");
+const Discord = require('discord.js');
 
+const tracker = new inviteTracker(client);
+
+module.exports = async (client, member, inviter) => {
+    
+  tracker.on("guildMemberAdd", async (member, inviter) => {
+    let logJoinLeave = db.get(`logjoinleave_${member.guild.id}`); // Fetching log pic channel ID from the database
+    let logChannel = member.guild.channels.cache.get(logJoinLeave);
+  
+    if (!logChannel) return;
+    if (!member.guild.id.includes(`${logChannel.guild.id}`)) return;
+    if (member.user.bot) return;
+
+    let serverMembersCount = member.guild.memberCount;
+
+    const fetchedLogs = await member.guild.fetchAuditLogs({
+      limit: 1,
+      type: 'BOT_ADD',
+    });
+
+    const botLog = fetchedLogs.entries.first();
+    if (!botLog) return;
+
+    const { executor } = botLog;
+    const invites = await member.guild.invites.fetch();
+    const inviterInvite = invites.find((invite) => invite.inviter.id === executor.id);
+
+    let devices = "Unknown";
+
+    if (member.presence) {
+      const deviceType = member.presence.clientStatus;
+
+      if (deviceType) {
+        if (deviceType.web) {
+          devices = "🌐 متصفح";
+        } else if (deviceType.desktop) {
+          devices = "💻 كمبيوتر";
+        } else if (deviceType.mobile) {
+          devices = "📱 جوال";
+        }
+      }
+    }
+
+    let inviterEmbed = new Discord.EmbedBuilder()
+      .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true, size: 1024, format: 'png' }))
+      .setThumbnail('https://cdn.discordapp.com/attachments/1064318878412451921/1179172938554019921/D8B5B65D-9A17-4CEF-A04E-7DA3B13985DD.png?ex=6578d160&is=65665c60&hm=402fec79be852f5f8dae69dd3fe42a2488fc64fb3adfec08f2146c6b27a15611&')
+      .setColor('#637a70')
+      .setDescription(`**انضمام العضو**\n\n**العضو : ${member && member.user ? `<@${member.user.id}>` : 'Unknown User'}**\n**بواسطة : ${inviter ? inviter : 'Unknown Inviter'}**\n**انضم فيـ : (<t:${Math.floor(member.joinedAt.getTime() / 1000)}:R>)**\n**الأجهزة : ${devices}**\n**عدد الأعضاء : ${serverMembersCount}**`)
+      .setFooter(inviter ? inviter.username : 'Unknown Inviter', inviter ? inviter.displayAvatarURL({ dynamic: true }) : '');
+
+    logChannel.send({ embeds: [inviterEmbed] });
+  });
+}
+*/
---
+++ events/Join/guildMemberRemove.js
@@ -0,0 +1,50 @@
+const db = require("pro.db");
+const humanizeDuration = require('humanize-duration');
+const Discord = require('discord.js');
+const { AuditLogEvent } = require('discord.js');
+
+module.exports = async (client, member) => {
+  if (member.user.bot) return;
+
+  let logjoinleave = db.get(`logjoinleave_${member.guild.id}`); // Fetching log pic channel ID from the database
+  var logChannel = member.guild.channels.cache.find(c => c.id === logjoinleave);
+  if (!logChannel) return;
+
+  let inviter;
+  const fetchedLogs = await member.guild.fetchAuditLogs({
+  limit: 1,
+  type: AuditLogEvent.MemberKick,
+});
+
+  const kickLog = fetchedLogs.entries.first();
+  if (!kickLog) {
+      const invites = await member.guild.invites.fetch();
+      const memberInvites = invites.filter(invite => invite.inviter && invite.inviter.id === member.id);
+      memberInvites.each(inv => {
+          if (inv.createdAt > (Date.now() - 86400000)) { // Check if the invite was created within the last 24 hours
+              inviter = inv.inviter;
+          }
+      });
+  } else {
+      inviter = kickLog.executor;
+  }
+
+  let inviterName = inviter ? inviter.id : "Unknown";
+
+let leaveMember = new Discord.EmbedBuilder()
+  .setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL({ dynamic: true, size: 1024, format: 'png' }) })
+      .setThumbnail('https://cdn.discordapp.com/attachments/1064318878412451921/1179172938331717682/AFB742D0-5B6A-4C25-BF91-FBA284280087.png?ex=6578d160&is=65665c60&hm=b6792253bc6fee3b37f039fba3abb2e75508daa5b6269270cf15aae8b25f7e26&')
+      .setColor(`#637a70`)
+      .setDescription(`**مغادرة العضو**\n\n**العضو : <@${member.user.id}>**\n**الداعي : <@${inviterName}>**\n**أنضم فيـ : (<t:${parseInt(member.user.createdAt / 1000)}:R>)**`)
+      .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL() });
+
+      if (member.roles.cache.size > 1) { 
+        const mentionedRoles = member.roles.cache
+            .filter(role => role.name !== '@everyone') // نستبعد رول الجميع
+            .map(role => role.toString()); // نقوم بتحويل كل رول إلى منشن
+    
+       leaveMember.addFields({ name: 'الأدوار السابقة', value: mentionedRoles.join(', ') });
+    }
+    
+  logChannel.send({ embeds: [leaveMember] });
+}
